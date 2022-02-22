1ff933f78b42f4e2d53f12c3f0e0a63d65965917
CaramelSuperman <98334952+CaramelSuperman@users.noreply.github.com>
2022-02-22 15:24:45 -0500

    right direction

--- residential_controller.js
+++ residential_controller.js
@@ -2,82 +2,84 @@ const { curry } = require("prelude-ls");
 
 class Column {
     constructor(_id, _amountOfFloors, _amountOfElevators) {
-        this._id = ID; 
-        this._amountOfFloors = _amountOfFloors;
-        this._amountOfElevators = _amountOfElevators;
-        let ID = 1;
-        let status = true;
-        let elevatorList = [];
-        let CallButtonList = [];
-        
+        this.elevatorList = [];
+        this.callButtonList = [];
+        this.ID = _id;
+        this.status = "online"
+        this.createElevators(_amountOfElevators)
+        this.createCallButtons(_amountOfFloors)
+    }
+
+    createElevators(amountOfElevators) {
+
+    }
+    
+    createCallButtons(amountOfFloors) {
 
     }
     
 
-    createElevator(){
+    requestELevator(currentFloor, direction) {
 
     }
 
-    requestELevator(currentFloor, direction){
-
-    }
-    findBestElevator(requestedFloor, requestedDirection){
+    findBestElevator(requestedFloor, requestedDirection) {
         let bestElevator = 0;
         let bestScore = 0;
         let floorDistanceList = [];
         let diference = 0;
 
     }
-    
-
 }
+
 
 class Elevator {
     constructor(_id, _amountOfFloors) {
-        this._id = _id;
-        this._amountOfFloors = _amountOfFloors;
-        let _id = 1;
-        let status;
-        let direction;
-        let currentFloor = 0;
-        'Door goes here'
-        let floorRequestButtonList = [];
-        let floorRequestList = [];
+        this.id = _id;
+        this.amountOfFloors = _amountOfFloors;
+        this.status;
+        this.direction;
+        this.currentFloor = 1;
+        //Door goes here'
+        this.floorRequestButtonList = [];
+        this.floorRequestList = [];
     }
-requestFloor(direction, floorRequestList, _id ){
 
-}
-move(_id){
-while(floorRequestList > 0){
-    for (var i = 0; i < floorRequestList.length;){
-        if (currentFloor < floorRequestList[0] ){
-           direction = 'up';
-           closeDoors(); 
-        }
-        //status = 'moving';
-        while(currentFloor < floorRequestList[0]){
-            currentFloor++
-          }
-          openDoors();
-           if(currentFloor > floorRequestList[0]){
-              direction = 'down';
-              closeDoors();
-            } 
-            //status - 'moving';
 
-            while (currentFloor > floorRequestList[0]){
-                currentFloor--
+    requestFloor(direction, floorRequestList, _id) {
+
+    }
+    move(requestedFloor, ) {
+        while (floorRequestList > 0) {
+            for (var i = 0; i < floorRequestList.length;) {
+                if (currentFloor < floorRequestList[0]) {
+                    direction = 'up';
+                    closeDoors();
+                }
+                //status = 'moving';
+                while (currentFloor < floorRequestList[0]) {
+                    currentFloor++
+                }
+                openDoors();
+                if (currentFloor > floorRequestList[0]) {
+                    direction = 'down';
+                    closeDoors();
+                }
+                //status - 'moving';
+
+                while (currentFloor > floorRequestList[0]) {
+                    currentFloor--
+                }
+
+                openDoors();
+                //status = 'stopped';
+                closeDoors();
             }
+            floorRequestList.shift();
+        }
 
-            openDoors();
-            //status = 'stopped';
-            closeDoors();
-        } 
-        floorRequestList.shift();
-}
-
-}
-        status = 'idle';
+    }
+    status = 'idle';
 }
 
 class CallButton {
@@ -93,34 +95,63 @@ class FloorRequestButton {
     constructor(_id, _floor) {
         this._id = _id;
         this._floor = _floor;
+        this.status = 'off';
     }
 }
 
 class Door {
     constructor(_id) {
-        this._id = ID;
-        ID = 1;
+        this._id = _id;
         this.status = "idle";
-        
+
 
     }
 
-    openDoors(){
+    openDoors() {
         this.status = "open"
+        console.log(statuts);
+    }
 
-}
-        
 
-    closeDoors(){
+    closeDoors() {
         this.status = "close"
     }
-    
 
 
-        
-}     
-    
 
+
+}
+
+const columnStatus = {
+    ACTIVE: 'active',
+    INACTIVE: 'inactive'
+};
+
+/* ******* ELEVATOR STATUS ******* */
+const elevatorStatus = {
+    IDLE: 'idle',
+    UP: 'up',
+    DOWN: 'down'
+};
+
+/* ******* BUTTON DIRECTION ******* */
+const buttonDirection = {
+    UP: 'up',
+    DOWN: 'down'
+};
+
+/* ******* BUTTONS STATUS ******* */
+const buttonStatus = {
+    ON: 'on',
+    OFF: 'off'
+};
+
+
+/* ******* DOORS STATUS ******* */
+const doorStatus = {
+    OPENED: 'opened',
+    CLOSED: 'closed'
+};
 
 
 module.exports = { Column, Elevator, CallButton, FloorRequestButton, Door }
